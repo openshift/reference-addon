f3f581c8caf5a5bb1a100b1e2b6d0899b2108468
Yashvardhan Kukreja <ykukreja@redhat.com>
2022-02-16 14:53:57 +0530

    cleanup some stuff
    Signed-off-by: Yashvardhan Kukreja <ykukreja@redhat.com>

--- internal/addoninstancesdk/clients.go
+++ internal/addoninstancesdk/clients.go
@@ -10,10 +10,6 @@ import (
 // The tenant can choose to implement the following methods in whatever way they want depending on the client-go/controller-runtime they possess
 type AddonInstanceInteractorClient interface {
 	GetAddonInstance(ctx context.Context, addonInstanceName string, addonInstanceNamespace string) (*addonsv1alpha1.AddonInstance, error)
-	// TODO: refine the following method
-	//AddonInstanceSpecChangeWatcher() (receiver chan addonsv1alpha1.AddonInstanceSpec, err error)
-	AddonInstanceSpecChangeWatcher()
-	// TODO: move condition as a part of the addonInstance body, addonInstance ~ desired addon instance with latest condition
 	UpdateAddonInstanceStatus(ctx context.Context, addonInstance *addonsv1alpha1.AddonInstance) error
 }
 
@@ -22,5 +18,3 @@ type AddonInstanceStatusReporterClient interface {
 	Start(ctx context.Context) error
 	SendHeartbeat(ctx context.Context, condition metav1.Condition) error
 }
-
-// SendHeartbeagt - client/tenant shouldn't make a k8s API call per se but signal it (somehow) to the heartbeat looper i.e. our sdk to update the condition

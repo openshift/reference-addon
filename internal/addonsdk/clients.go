d52fda6f9721724d805e6f26761a9af100972dbe
Yashvardhan Kukreja <ykukreja@redhat.com>
2022-02-28 18:10:06 +0530

    nits
    Signed-off-by: Yashvardhan Kukreja <ykukreja@redhat.com>

--- internal/addonsdk/clients.go
+++ internal/addonsdk/clients.go
@@ -7,10 +7,10 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 )
 
-// our tenants need to build a struct to implement this (here in reference addon, that struct is in `internal/referenceaddoninteractor/`)
+// our tenants need to build a struct to implement this (here in reference addon, that struct is in `cmd/reference-addon-manager/addonsdkclient.go`)
 // The tenant can choose to implement the following methods in whatever way they want depending on the client-go/controller-runtime they possess
 type client interface {
 	// the following GetAddonInstance method should be backed by a cache
-	GetAddonInstance(context.Context, types.NamespacedName, *addonsv1alpha1.AddonInstance) error
+	GetAddonInstance(ctx context.Context, key types.NamespacedName, addonInstance *addonsv1alpha1.AddonInstance) error
 	UpdateAddonInstanceStatus(ctx context.Context, addonInstance *addonsv1alpha1.AddonInstance) error
 }

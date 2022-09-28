a9365cae57e51603d50711606dce9fac07986794
Andrew Pantuso <apantuso@redhat.com>
2022-09-28 15:37:27 -0400

    ci: add magefile and cleanup MakeFile

---
+++ tools/tools.go
@@ -0,0 +1,8 @@
+package main
+
+import (
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/onsi/ginkgo/v2/ginkgo"
+	_ "sigs.k8s.io/controller-runtime/tools/setup-envtest"
+	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
+)

1beef5fcba3a9391495603ec963f84beae98161a
Sandhya Dalavi <sdalavi@redhat.com>
2023-01-26 16:26:21 -0500

    initial reference-addon/addoninstance status

--- internal/controllers/referenceaddon/status_controller.go
+++ internal/controllers/referenceaddon/status_controller.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/go-logr/logr"
 	av1alpha1 "github.com/openshift/addon-operator/apis/addons/v1alpha1"
 	addoninstance "github.com/openshift/addon-operator/pkg/client"
+	rv1alpha1 "github.com/openshift/reference-addon/apis/reference/v1alpha1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -31,7 +33,7 @@ func NewStatusControllerReconciler(client client.Client) *StatusControllerReconc
 
 func (r *StatusControllerReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&av1alpha1.AddonInstance{}).
+		For(&rv1alpha1.ReferenceAddon{}).
 		Complete(r)
 }
 
@@ -42,13 +44,21 @@ func (s *StatusControllerReconciler) Reconcile(ctx context.Context, req ctrl.Req
 		Name:      req.Name,
 	}
 
+	log.Info("find reference addon: ", req.Namespace, req.Name)
+	referenceaddon := &rv1alpha1.ReferenceAddon{}
+	if err := s.client.Get(ctx, key, referenceaddon); err != nil {
+		return ctrl.Result{}, fmt.Errorf("getting Reference-Addon Object'%s/%s': %w", req.Namespace, req.Name, err)
+	}
+
 	log.Info("find addon instance: ", req.Namespace, req.Name)
 	addonInstance := &av1alpha1.AddonInstance{}
 	if err := s.client.Get(ctx, key, addonInstance); err != nil {
-		return ctrl.Result{}, fmt.Errorf("getting AddonInstance '%s/%s': %w", req.Namespace, req.Name, err)
+		return ctrl.Result{}, fmt.Errorf("getting AddonInstance Object'%s/%s': %w", req.Namespace, req.Name, err)
 	}
-
 	log.Info("found addon instance: ", addonInstance)
+
+	getAdonInstanceUpdateCondition(referenceaddon, addonInstance, log)
+
 	err := s.addonInstanceClient.SendPulse(ctx, *addonInstance, nil)
 	if err != nil {
 		return ctrl.Result{}, err
@@ -57,4 +67,10 @@ func (s *StatusControllerReconciler) Reconcile(ctx context.Context, req ctrl.Req
 	log.Info("successfully reconciled AddonInstance")
 
 	return ctrl.Result{RequeueAfter: defaultRetryAfterTime}, nil
+}
+
+func getAdonInstanceUpdateCondition(referenceaddon *rv1alpha1.ReferenceAddon, addonInstance *av1alpha1.AddonInstance, log logr.Logger) {
+	if referenceaddon.HasConditionAvailable() {
+		log.Info("condition available")
+	}
 }
